2039f9118a7c11aa95f8ed3db94c8e464130ec9f
OLEkSandrKoniaiev <128646916+OLEkSandrKoniaiev@users.noreply.github.com>
2025-07-21 17:02:07 +0300

    add user.module.ts which includes IUser, ICreateUserDTO & IPublicUserDTO

---
+++ src/types/user.types.ts
@@ -0,0 +1,19 @@
+export interface IUser {
+  id: number;
+  name: string;
+  email: string;
+  password?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+// DTO (Data Transfer Object)
+export interface ICreateUserDTO {
+  // name: string;
+  email: string;
+  password: string;
+}
+
+// export type IUpdateUserDTO = Partial<ICreateUserDTO>;
+
+export type IPublicUserDTO = Omit<IUser, 'id' | 'password'>;
