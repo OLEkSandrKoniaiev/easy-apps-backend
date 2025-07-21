a57449d134449f7d9db2a12a3cfacc7baf8d61a6
OLEkSandrKoniaiev <128646916+OLEkSandrKoniaiev@users.noreply.github.com>
2025-07-21 15:49:18 +0300

    split index.ts into index.ts & app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import dotenv from 'dotenv';
-import express from 'express';
+import app from './app';
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 3000;
-
-app.get('/', (_req, res) => {
-  res.send('Hello World');
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
