e0c0a4ee12bfb3c04193961479437d2e65767fa2
OLEkSandrKoniaiev <128646916+OLEkSandrKoniaiev@users.noreply.github.com>
2025-07-21 19:08:10 +0300

    update app.ts & index.ts files; ConnectDB func is now in index.ts; add try-catch start func for index.ts; add NODE_ENV param to .env file for better morgan

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import morgan from 'morgan';
-import { connectDB } from './configs/database.config';
-
-connectDB();
 
 const app = express();
 
-// Підключаємо Morgan middleware
-app.use(morgan('dev'));
+// Підключаємо Morgan middleware залежно від середовища
+if (process.env.NODE_ENV === 'production') {
+  app.use(morgan('combined'));
+} else {
+  app.use(morgan('dev'));
+}
 
 app.get('/', (_req, res) => {
   res.send('Hello World');
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import dotenv from 'dotenv';
+import { connectDB } from './configs/database.config';
 import app from './app';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+const start = async (): Promise<void> => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running at http://localhost:${PORT}`);
+    });
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error('Unable to connect to the database:', error.message);
+    } else {
+      console.error(
+        'An unknown error occurred while connecting to the database or app listening:',
+        error,
+      );
+    }
+  }
+};
+
+start();
