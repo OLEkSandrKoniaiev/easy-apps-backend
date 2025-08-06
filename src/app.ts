a9bf93b90a108568ae4f44117cdb9e1c44c47981
OLEkSandrKoniaiev <oleksandrkoniaiev.r@gmail.com>
2025-08-06 13:27:06 +0300

    feat: add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,13 @@ import express from 'express';
 import morgan from 'morgan';
 import authRoutes from './routes/auth.routes';
 import userRoutes from './routes/user.routes';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 // Підключаємо Morgan middleware залежно від середовища
 if (process.env.NODE_ENV === 'production') {
