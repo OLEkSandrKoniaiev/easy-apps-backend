920feea7963969bc76a577c47ff012a7cfe4cabe
OLEkSandrKoniaiev <oleksandrkoniaiev.r@gmail.com>
2025-08-09 02:26:34 +0300

    feat: add new deleteById method to TaskRepository

--- src/repositories/task.repository.ts
+++ src/repositories/task.repository.ts
@@ -6,4 +6,13 @@ export class TaskRepository {
     const task = await TaskModel.create(data);
     return task.get();
   }
+
+  static async findById(taskId: number): Promise<ITask | null> {
+    return TaskModel.findOne({ where: { id: taskId } });
+  }
+
+  static async deleteById(taskId: number): Promise<boolean> {
+    const deletedCount = await TaskModel.destroy({ where: { id: taskId } });
+    return deletedCount > 0;
+  }
 }
